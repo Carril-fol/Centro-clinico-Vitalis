d7891de599632110761640eb3d40beac22d58dd0
Folco Carril <folco.carril@gmail.com>
2024-10-22 03:17:19 -0300

    refactor: Se cambio la dirección del archivo

---
+++ components/turns/table/TurnTableComponent.php
@@ -0,0 +1,48 @@
+<?php
+    // Importes
+    require_once '../../controllers/turn/TurnController.php';
+    require_once 'TurnDataTableComponent.php';
+
+    // Turnos 
+    $turns = $turnController->showTurnsAvailable();
+    
+    function renderTurnTable($turns) {
+        if (empty($turns)) {
+            echo "
+                <div>
+                    No hay turnos por el momento a mostrar
+                </div>
+            ";
+        } else {
+            echo "
+                <div class='container-table-button-top'>
+                    <a href='../../views/turn/createTurn.php'>
+                        <button class='table-button-add'>
+                            Agregar
+                        </button>
+                    </a>
+                </div>
+            ";
+            echo "
+                <table class='content-table'>
+                    <thead>
+                        <tr>
+                            <th>DNI - Paciente</th>
+                            <th>DNI - Medico</th>
+                            <th>Fecha atención</th>
+                            <th>Fecha creación</th>
+                            <th>Horario</th>
+                            <th>Estado</th>
+                            <th>Acciones</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                ";
+                renderDataTable($turns);
+            echo "  </tbody>
+                </table>";
+        }
+    }
+
+    renderTurnTable($turns);
+?>
