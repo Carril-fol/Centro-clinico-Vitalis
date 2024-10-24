ca0c23fb043e01e42d5bedf263f405895748f0ac
Folco Carril <folco.carril@gmail.com>
2024-10-24 02:24:42 -0300

    feat: Se agrego un formulario que permite la actualización de los turno por parte de los administrativos.

---
+++ components/turns/forms/TurnFormUpdateComponent.php
@@ -0,0 +1,45 @@
+<?php
+// Importe de controlador
+require_once '../../controllers/turn/TurnController.php';
+
+// Instancia de controlador
+$turnController = new TurnController();
+
+// Llamado de función del modelo de Medico
+$specialities = $turnController->medicModel->getSpecialitiesAvailable();
+
+// Llamado a función del controlador
+$turnData = $turnController->detailTurn();
+
+function optionsFormated($turnData, $specialities) {
+    ?>
+        <option value='<?php echo htmlspecialchars($turnData['especialidad']); ?>' selected>
+            <?php echo htmlspecialchars($turnData['especialidad']); ?>
+        </option>
+    <?php
+    foreach ($specialities as $row): 
+        ?>
+            <option value='<?php echo htmlspecialchars($row['especialidad']); ?>'>
+                <?php echo htmlspecialchars($row['especialidad']); ?>
+            </option>
+        <?php
+    endforeach;
+}
+
+?>
+<!-- Formulario -->
+<form class='formulario-turno-creacion' method='POST' action='../../controllers/turn/TurnController.php?action=update&id=<?php echo $turnData['id']; ?>'>
+    <div>
+        <img src='../../assets/images/logo.webp' alt='Logo'/>
+        <h1>Actualizar turno</h1>
+    </div>
+    <input type='text' name='dniPatient' value='<?php echo htmlspecialchars($turnData['dni_paciente']); ?>'>
+    <input type='date' name='dateAtention' value='<?php echo htmlspecialchars($turnData['fecha_atencion']); ?>'>
+    <input type='time' name='timeAtention' value='<?php echo htmlspecialchars($turnData['horario']); ?>'>
+    <select required name='speciality'>
+        <?php optionsFormated($turnData, $specialities); ?>
+    </select>
+    <button type='submit'>
+         Actualizar
+    </button>
+</form>
