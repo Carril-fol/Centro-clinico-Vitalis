28e315f5ae2812c320d0e5e44db6fab80896c6b3
Folco Carril <folco.carril@gmail.com>
2024-10-29 00:41:50 -0300

    feat: Se agregaron dos funcionalidades nuevas en el controller.

--- controllers/auth/LoginController.php
+++ controllers/auth/LoginController.php
@@ -21,6 +21,22 @@
             }
         }
 
+        private function createCookieData($dataUser){
+            $timeExp = time() + 86400;
+            $cookieData = array('data'=>$dataUser, 'exp'=>$timeExp);
+            setcookie('accessToken', json_encode($cookieData), $timeExp, "/");
+        }
+
+        private function createRolCookie($dni) {
+            $userData = $this->userModel->getRolFromUserByDni($dni);
+            if (!$userData) {
+                throw new Exception("The entered user cannot be found.");
+            }
+            $timeExp = time() + 86400;
+            $cookieData = array('data'=>$userData['rol'], 'exp'=>$timeExp);
+            setcookie("userRol", json_encode($cookieData), $timeExp, "/");
+        }
+
         private function redirectToHome() {
             header("Location: ../../views/core/home.php");
             exit();
@@ -30,12 +46,11 @@
             try {
                 $dni = $this->sanitizeInput($this->dni);
                 $password = $this->sanitizeInput($this->password);
-                
                 $this->verifyCredentials($dni, $password);
     
                 $userDataLogged = $this->userModel->getDataFromUserByDni($dni);
-                $this->userModel->createCookieData($userDataLogged);
-                
+                $this->createCookieData($userDataLogged);
+                $this->createRolCookie($this->dni);
                 $this->redirectToHome();
             } catch (Exception $error) {
                 $this->handleError($error, 'auth', 'login');
