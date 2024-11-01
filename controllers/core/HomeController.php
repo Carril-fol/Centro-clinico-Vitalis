6f1e063f209a2a91065f6a470443486c0727391c
Folco Carril <folco.carril@gmail.com>
2024-10-31 20:19:27 -0600

    feat: Función que permite ver los roles de los usuarios guardados en las cookies.

--- controllers/core/HomeController.php
+++ controllers/core/HomeController.php
@@ -1,21 +1,25 @@
 <?php
-    // Importes
-    require_once 'Controller.php';
+// Importes
+require_once 'Controller.php';
 
-    class HomeController extends Controller
+class HomeController extends Controller
+{
+    public function rolFromUser()
     {
-
-        public function hasAccessTokenInCookies() {
-            try {
-                $cookie = $_COOKIE["accessToken"];
-                if (!isset($cookie)) {
-                    throw new Exception("Tienes que iniciar sesión para poder accerder");
-                }
-                return true;
-            } catch (Exception $error) {
-                $this->handleError($error, 'auth', 'login');
-            }
-        }
-
+        $cookieRol = strtolower(json_decode($_COOKIE["userRol"], true)['data']);
+        return $cookieRol;
     }
-?>
+
+    public function hasAccessTokenInCookies()
+    {
+        try {
+            $cookie = $_COOKIE["accessToken"];
+            if (!isset($cookie)) {
+                throw new Exception("Tienes que iniciar sesión para poder accerder");
+            }
+            return true;
+        } catch (Exception $error) {
+            $this->handleError($error, 'auth', 'login');
+        }
+    }
+}
