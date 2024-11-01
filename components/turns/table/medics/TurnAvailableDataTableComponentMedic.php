7d288468dfff49700c9b1ed34ea14a4ba5c007c2
Folco Carril <folco.carril@gmail.com>
2024-10-31 20:16:16 -0600

    feat: Componentes agregados para su uso.

---
+++ components/turns/table/medics/TurnAvailableDataTableComponentMedic.php
@@ -0,0 +1,36 @@
+<?php
+// Importes
+require_once '../../controllers/medic/MedicController.php';
+
+$medicController = new MedicController();
+
+$turns = $medicController->showAllTurnAvailableFromMedic();
+?>
+<?php if (!empty($turns)): ?>
+    <?php foreach ($turns as $turn): ?>
+        <tr>
+            <td><?php echo htmlspecialchars($turn["dni_paciente"]); ?></td>
+            <td><?php echo htmlspecialchars($turn['fecha_atencion']); ?></td>
+            <td><?php echo htmlspecialchars($turn['fecha_creacion']); ?></td>
+            <td><?php echo htmlspecialchars($turn['horario']); ?></td>
+            <td><?php echo htmlspecialchars($turn['estado']); ?></td>
+            <td>
+                <div class='container-buttons-table-aside'>
+                    <a href='../../controllers/medic/MedicController.php?action=update&id=<?php echo $turn['id']; ?>'>
+                        <button class='table-button-update' type='button'>Completar</button>
+                    </a>
+                    <a href='../../views/turn/update.php?action=update&id=<?php echo $turn['id']; ?>'>
+                        <button class='table-button-update' type='button'>Editar</button>
+                    </a>
+                    <a href='../../controllers/turn/TurnController.php?action=delete&id=<?php echo $turn['id']; ?>'>
+                        <button class='table-button-delete' type='button'>Eliminar</button>
+                    </a>
+                </div>
+            </td>
+        </tr>
+    <?php endforeach; ?>
+<?php else: ?>
+    <tr>
+        <td colspan="7">No tienes turnos por el momento.</td>
+    </tr>
+<?php endif; ?>
