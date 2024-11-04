466ef749fb436f8d5162c4f6f43e41851dceb60a
Folco Carril <folco.carril@gmail.com>
2024-11-04 07:36:29 -0600

    feat: Se creo un nuevo modelo para el manejo de los turnos solicitados por los pacientes.

---
+++ models/TurnRequested.php
@@ -0,0 +1,67 @@
+<?php
+require_once  __DIR__ . '/../config.php';
+require_once "Turn.php";
+
+class TurnRequested extends Turn
+{
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function setProperties($id, $dniPatient, $dateAtention, $dateCreation, $turnTime, $status, $speciality) {
+        $this->id = $id;
+        $this->dniPatient = $dniPatient;
+        $this->dateAtention = $dateAtention;
+        $this->dateCreation = $dateCreation;
+        $this->turnTime = $turnTime;
+        $this->status = $status;
+        $this->speciality = $speciality;
+    }
+
+    public function createTurnRequested()
+    {
+        $paramsQuery = [
+            ":dniPatient" => $this->dniPatient,
+            ":dateAtention" => $this->dateAtention,
+            ":dateCreation" =>  $this->dateCreation,
+            ":turnTime" => $this->turnTime,
+            ":status" => $this->status,
+            ":speciality" => $this->speciality
+        ];
+        $insertQuery = "INSERT INTO turnos_solicitados (
+            dni_paciente,
+            fecha_atencion,
+            fecha_creacion,
+            horario,
+            estado,
+            especialidad
+        ) VALUES (
+            :dniPatient,
+            :dateAtention,
+            :dateCreation,
+            :turnTime,
+            :status,
+            :speciality
+        )";
+        $resultQuery = $this->db->prepare($insertQuery);
+        return $resultQuery->execute($paramsQuery);
+    }
+
+    public function detailFromTurnRequestedById()
+    {
+        $paramsQuery = [":id" => $this->id];
+        $selectQuery = "SELECT * FROM turnos_solicitados WHERE id = :id";
+        $resultQuery = $this->db->prepare($selectQuery);
+        $resultQuery->execute($paramsQuery);
+        return $resultQuery->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateStatusTurnRequestedById()
+    {
+        $paramsQuery = [":id" => $this->id, ":status" => $this->status];
+        $updateQuery = "UPDATE turnos_solicitados SET estado = :status WHERE id = :id";
+        $resultQuery = $this->db->prepare($updateQuery);
+        $resultQuery->execute($paramsQuery);
+        return $resultQuery->rowCount();
+    }
+}
