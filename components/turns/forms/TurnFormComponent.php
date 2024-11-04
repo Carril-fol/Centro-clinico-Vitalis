4d3431c5242395e25d33026f20393ce3aebc375c
Folco Carril <folco.carril@gmail.com>
2024-11-03 19:22:31 -0600

    feat: Se agrego una validación en el formulario para restringir a los usuarios que no son administrativos.

--- components/turns/forms/TurnFormComponent.php
+++ components/turns/forms/TurnFormComponent.php
@@ -1,45 +1,59 @@
 <?php
-// Importe de controlador
-require_once '../../controllers/turn/TurnController.php';
+require_once "../../controllers/turn/TurnController.php";
+require_once "../../controllers/core/HomeController.php";
 
-// Instancia de controlador
 $turnController = new TurnController();
+$homeController = new HomeController();
 
-// Llamado de función del modelo de Medico
-$specialities = $turnController->medicModel->getSpecialitiesAvailable();
+$rolFromUser = $homeController->getRolFromUser();
+$validationRolFromUser = $homeController->validateRolFromUser($rolFromUser, "administrativo");
 
-function optionsFormated($specialities) {
+function optionsFormated() {
+    global $turnController;
+    $specialities = $turnController->medicModel->getSpecialitiesAvailable();
     if (empty($specialities)) {
         echo "<option value=''>No hay especialidades disponibles.</option>";
     } else {
-        foreach ($specialities as $row): 
+        foreach ($specialities as $row) {
             ?>
-            <option value='<?= htmlspecialchars($row['especialidad']); ?>'>
-                <?= htmlspecialchars($row['especialidad']); ?>
-            </option>
+                <option value='<?= htmlspecialchars($row['especialidad']); ?>'>
+                    <?= htmlspecialchars($row['especialidad']); ?>
+                </option>
             <?php
-        endforeach;
+        }
     }
 }
 ?>
-<form class='formulario-turno-creacion' method='POST' action='../../controllers/turn/TurnController.php?action=create'>
-    <div>
-        <img src='../../assets/images/logo.webp' alt='Logo'/>
-        <h1>Crea tu turno</h1>
+<form class="form-turn-creation" method="POST" action="../../controllers/turn/TurnController.php?action=create">
+    <div class="container-fields-form-turn">
+        <div class="form-header">
+            <h1>Registro de Turnos para Pacientes</h1>
+        </div>
+
+        <div class="container-field">
+            <label for="dniPatient">Dni del paciente</label>
+            <input type='text' name='dniPatient'>
+        </div>
+
+        <div class="container-field">
+            <label for="dateAtention">Fecha de turno</label>
+            <input type="date" id="dateAtention" name="dateAtention" required placeholder="Selecciona la fecha">
+        </div>
+
+        <div class="container-field">
+            <label for="timeAtention">Hora de turno</label>
+            <input type="time" id="timeAtention" name="timeAtention" min="09:00" max="18:00" required>
+        </div>
+
+        <div class="container-field">
+            <label for="speciality">Especialidad</label>
+            <select id="speciality" name="speciality" required>
+                <option value="" disabled selected>Selecciona una especialidad</option>
+                <?php optionsFormated($specialities); ?>
+            </select>
+        </div>
+        <div class="container-form-button-submit">
+            <button type="submit" class="submit-button">Crear</button>
+        </div>
     </div>
-    <input type='text' name='dniPatient' placeholder='DNI DEL PACIENTE'>
-    <div>
-        <label for="dateAtention">Fecha de turno</label>
-        <input type='date' name='dateAtention'>
-    </div>
-    <div>
-        <label for="timeAtention"></label>
-        <input type='time' name='timeAtention'>
-    </div>
-    <select required name='speciality'>
-        <?php optionsFormated($specialities); ?>
-    </select>
-    <button type='submit'>
-        Crear
-    </button>
 </form>
