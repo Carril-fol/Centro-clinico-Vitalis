7e5431cf14d075e40177328c2f19ca92406a92cd
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-08 01:49:37 -0600

    feat: Se agregaron funciones nuevas.

--- controllers/medic/MedicController.php
+++ controllers/medic/MedicController.php
@@ -1,5 +1,6 @@
 <?php
 require_once '../../models/Turn.php';
+require_once '../../models/TurnRequested.php';
 require_once '../../models/Medic.php';
 require_once '../../controllers/core/Controller.php';
 
@@ -7,11 +8,13 @@ class MedicController extends Controller
 {
     private $medicModel;
     private $turnModel;
+    private $turnRequestedModel;
 
     function __construct()
     {
         $this->medicModel = new Medic();
         $this->turnModel = new Turn();
+        $this->turnRequestedModel = new TurnRequested();
     }
 
     private function redirectToHome()
@@ -20,10 +23,48 @@ class MedicController extends Controller
         exit();
     }
 
-    public function returnTurnStatus()
+    private function actualizeStatusTurnRequested($idTurnParent)
     {
-        $turn = $this->turnModel->detailTurnById()['estado'];
-        return $turn;
+        $this->turnModel->setId($idTurnParent);
+        $dniPatient = $this->turnModel->detailTurnById()["dni_paciente"];
+        
+        $dataTurn = $this->turnRequestedModel->detailTurnRequestedConfirmedFromPatientByDni($dniPatient);
+        if ($dataTurn == false) {
+            return;
+        }
+
+        if ($dataTurn["estado"] == "COMPLETADO") {
+            $this->turnRequestedModel->setStatus("CONFIRMADO");
+        } else {
+            $this->turnRequestedModel->setStatus("COMPLETADO");
+        }
+
+        $this->turnRequestedModel->setId($dataTurn["id"]);
+        $this->turnRequestedModel->updateStatusTurnRequestedById();
+    }
+
+    private function actualizeStatusTurn($idTurn)
+    {
+        $this->turnModel->setId($idTurn);
+        $dataTurn = $this->turnModel->detailTurnById();
+    
+        if ($dataTurn["estado"] == "COMPLETADO") {
+            $this->turnModel->setStatus("PENDIENTE");
+        } else {
+            $this->turnModel->setStatus("COMPLETADO");
+        }
+        $this->turnModel->updateStatusTurnById();
+        $dataUpdatedTurn = $this->turnModel->detailTurnById();
+        return $dataUpdatedTurn;
+    }
+
+    private function actualizeStatusMedic($dniMedic, $statusTurn)
+    {
+        if ($statusTurn == "COMPLETADO") {
+            $this->medicModel->changeStatusMedic($dniMedic, "DESOCUPADO");
+        } elseif ($statusTurn == "PENDIENTE") {
+            $this->medicModel->changeStatusMedic($dniMedic, "OCUPADO");
+        }
     }
 
     public function showAllTurnAvailableFromMedic()
@@ -40,30 +81,26 @@ class MedicController extends Controller
         return $turnsFromMedic;
     }
 
-    public function updateTurn()
+    public function updateTurns()
     {
         try {
             $id = $this->getIdUrl();
-            $this->turnModel->setId($id);
-            $turnStatus = $this->returnTurnStatus();
-            if ($turnStatus == "COMPLETADO") {
-                $this->turnModel->setStatus("PENDIENTE");
-            } else {
-                $this->turnModel->setStatus("COMPLETADO");
-            }
-            $this->turnModel->updateStatusTurnById();
+            $turnDataUpdated = $this->actualizeStatusTurn($id);
+            $this->actualizeStatusMedic($turnDataUpdated["dni_medico"], $turnDataUpdated["estado"]);
+            $this->actualizeStatusTurnRequested($id);
             $this->redirectToHome();
         } catch (Exception $error) {
             $this->handleError($error, "core", "home");
         }
     }
+
 }
 
-$medicController = new MedicController();
-$action = strtoupper($medicController->getActionInUrl());
+$controller = new MedicController();
+$action = strtoupper($controller->getActionInUrl());
 
 switch ($action) {
     case "UPDATE":
-        $medicController->updateTurn();
+        $controller->updateTurns();
         break;
 }
