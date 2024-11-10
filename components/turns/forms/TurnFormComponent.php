2e23f7e13650b68bdebf73a81713b5509293f72f
Folco Carril <107985251+Carril-fol@users.noreply.github.com>
2024-11-09 22:23:27 -0600

    feat: Se agrego una funcionalidad que permite ver los horas disponibles para los turnos.

--- components/turns/forms/TurnFormComponent.php
+++ components/turns/forms/TurnFormComponent.php
@@ -8,19 +8,31 @@ $homeController = new HomeController();
 $rolFromUser = $homeController->getRolFromUser();
 $validationRolFromUser = $homeController->validateRolFromUser($rolFromUser, "administrativo");
 
-function optionsFormated() {
+function optionsFormated()
+{
     global $turnController;
     $specialities = $turnController->medicModel->getSpecialitiesAvailable();
     if (empty($specialities)) {
         echo "<option value=''>No hay especialidades disponibles.</option>";
     } else {
         foreach ($specialities as $row) {
-            ?>
-                <option value='<?= htmlspecialchars($row['especialidad']); ?>'>
-                    <?= htmlspecialchars($row['especialidad']); ?>
-                </option>
-            <?php
+?>
+            <option value='<?= htmlspecialchars($row['especialidad']); ?>'>
+                <?= htmlspecialchars($row['especialidad']); ?>
+            </option>
+<?php
         }
+    }
+}
+
+function optionsTimeFormated()
+{
+    $timeStart = new DateTime("9:00");
+    $timeEnd = new DateTime("18:00");
+
+    for ($time = $timeStart; $time <= $timeEnd; $time->modify("+30 minutes")) {
+        $timeValue = $time->format("H:i");
+        echo "<option value='$timeValue'>$timeValue</option>";
     }
 }
 ?>
@@ -37,12 +49,14 @@ function optionsFormated() {
 
         <div class="container-field">
             <label for="dateAtention">Fecha de turno</label>
-            <input type="date" id="dateAtention" name="dateAtention" required placeholder="Selecciona la fecha">
+            <input type="date" id="dateAtention" name="dateAtention" required placeholder="Selecciona la fecha" min="<?php echo date('Y-m-d'); ?>" max="<?php echo date('Y-12-31'); ?>">
         </div>
 
         <div class="container-field">
             <label for="timeAtention">Hora de turno</label>
-            <input type="time" id="timeAtention" name="timeAtention" min="09:00" max="18:00" required>
+            <select type="time" id="timeAtention" name="timeAtention" required>
+                <?php optionsTimeFormated() ?>
+            </select>
         </div>
 
         <div class="container-field">
